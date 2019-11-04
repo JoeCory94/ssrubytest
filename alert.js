e5c707e1055c09f2f0532f67c2b495f35801e65a
JoeCory94 <joecory94@gmail.com>
2019-11-04 11:22:06 +0000

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,1 +1,18 @@
-alert('this has loaded')
+$(".counter").each(function() {
+  var a = $(this),
+      b = a.attr("data-count");
+  $({
+    countNum: a.text()
+  }).animate({
+    countNum: b
+  }, {
+    duration: 3e3,
+    easing: "linear",
+    step: function() {
+      a.text(Math.floor(this.countNum))
+    },
+    complete: function() {
+      a.text(this.countNum)
+    }
+  })
+})
